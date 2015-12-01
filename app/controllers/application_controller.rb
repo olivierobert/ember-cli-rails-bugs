1eab09d9fb172d884455a8cfd206cd48a19c92cd
Olivier Robert <olivierobert@gmail.com>
2015-12-01 21:31:40 +0700

    [CHORE] Setup custom Ember controller and view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Ember mounted app
+  # Required to be defined here as of ember-cli-rails v0.5.6
+  def backend
+    render 'backend/index', layout: false
+  end
 end
